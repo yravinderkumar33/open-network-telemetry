1c649e77bf99d1f7c912b08261affaa3ccd441b5
shiva-rakshith <rakshiths@sanketika.in>
2024-02-13 12:53:55 +0530

    Update TelemetryMiddleware.js

--- open-network-telemetry-sdk/src/impl/TelemetryMiddleware.js
+++ open-network-telemetry-sdk/src/impl/TelemetryMiddleware.js
@@ -5,7 +5,6 @@ export const telemetryMiddleware = (inputConfig) => {
 
     Telemetry.init(inputConfig);
 
-
     const generate = async (req, res, next) => {
         res.body.statusCode = res?.statusCode;
         Telemetry.generate(req.body, res.body);
